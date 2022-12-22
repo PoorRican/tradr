4b694e478e4276f2b7ac9f050ee2f4a92592217e
Josue Figueroa <poor.rican@pm.me>
2022-12-21 20:16:58 -0800

    Fix plotting of assets/capital

--- analysis/plot.py
+++ analysis/plot.py
@@ -11,12 +11,14 @@ from strategies import ThreeProngAlt
 
 
 class Location(IntEnum):
+    """ Abstracts sub-plot indexes """
     PRIMARY = 0
     ASSETS = 1
 
 
 # TODO: create parent object that serves as base wrapper for ThreeProngAlt models
 class Plotter(object):
+    """ Wrapper that plots `ThreeProngAlt` models """
     rows = 4
 
     def __init__(self, model: ThreeProngAlt):
@@ -76,8 +78,6 @@ class Plotter(object):
             endpoints = {}
             orders = self.model.orders
 
-        # plot settings
-
         pri = self.subplots[Location.PRIMARY]
         pri.plot(candles.index, candles['close'], color=to_rgba('blue', 0.8))
 
@@ -89,17 +89,25 @@ class Plotter(object):
             plt.show()
 
     def _plot_money(self, index: pd.Index, assets: pd.Series, capital: pd.Series):
+        """ Plot acquired capital and assets """
         sec = self.subplots[1]
+        _last = assets.iloc[-1]
         _assets: pd.Series = assets.copy()
-        _assets.reindex(index, copy=False)
+        _assets = _assets.reindex(index, fill_value=None)
+        _assets.iloc[-1] = _last
+        _assets = _assets.interpolate()
         sec.plot(_assets.index, _assets, color="purple")
 
         sec2 = sec.twinx()
+        _last = capital.iloc[-1]
         _capital = capital.copy()
-        _capital.reindex(index, copy=False)
+        _capital = _capital.reindex(index, fill_value=None)
+        _capital.iloc[-1] = _last
+        _capital = _capital.interpolate()
         sec2.plot(_capital.index, _capital.values, color="green")
 
     def _plot_decisions(self, orders: pd.DataFrame):
+        """ Plot trade enter and exit locations. """
         figure = self.subplots[Location.PRIMARY]
         size = 10
         scalar = 4
