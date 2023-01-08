4763ec4cf3809d4c80ce91c4141c0764a311c781
Josue Figueroa <poor.rican@pm.me>
2023-01-08 02:37:18 -0800

    Improve quantizing in  `BBANDSROW.strength()` #75

--- models/indicators/BBANDSRow.py
+++ models/indicators/BBANDSRow.py
@@ -19,12 +19,28 @@ class BBANDSRow(Indicator):
 
         self.threshold = threshold
 
-    def _calculate_thresholds(self, row: Union['pd.Series', 'pd.DataFrame']) -> Tuple[float, float]:
+    def _calculate_thresholds(self, row: Union['pd.Series', 'pd.DataFrame'],
+                              threshold: float = None) -> Tuple[float, float]:
+        """ Calculate and return threshold values for buy and sell signals.
+
+        Buy signal is generated when price is lower than buy threshold;  sell signal is generated when
+        price is higher than sell threshold.
+
+        Args:
+            threshold:
+                Optional value to override default instance value.
+
+                "Width" gets wider as the value of `threshold` increases.  A value of 1 would be equal to "upper" and
+                "lower" bands.  A value of 0 would be equal to "middle" band.  Therefore, a higher value of threshold
+                selects for more divergent values.
+        """
+        if threshold is None:
+            threshold = self.threshold
         buy = row['middleband'] - row['lowerband']
         sell = row['upperband'] - row['middleband']
 
-        buy *= 1 - self.threshold
-        sell *= self.threshold
+        buy *= 1 - threshold
+        sell *= threshold
 
         buy += row['lowerband']
         sell += row['middleband']
@@ -35,6 +51,12 @@ class BBANDSRow(Indicator):
         return buy, sell
 
     def _extract_rate(self, row: Union['pd.Series', 'pd.DataFrame'], candles: pd.DataFrame) -> float:
+        """ Extract `rate` from `row`.
+
+        Notes:
+            This function is necessary because indexing daily data returns a dataframe, due to indexing a
+            `DatetimeIndex` with a `str` instead of a `Timestamp` object. Normally,
+        """
         if not hasattr(row, 'name'):
             point = row.index[0]
         else:
@@ -58,28 +80,30 @@ class BBANDSRow(Indicator):
         rate = self._extract_rate(row, candles)
 
         buy, sell = self._calculate_thresholds(row)
-
-        # TODO: scalar should be calculated as quantized distance. Rates closer to midline
-        #   should have lower strength
+        buy2, sell2 = self._calculate_thresholds(row, .5)
+        buy3, sell3 = self._calculate_thresholds(row, .75)
 
         if isnan(buy) or isnan(sell):
             return nan
-        elif rate <= buy:
+        elif rate < buy:
             lower = row['lowerband']
-            diff = buy - lower
-            if rate > lower:
-                return 1
-            elif rate > lower - diff:
+
+            if rate < lower:
+                return 4
+            elif rate < buy3:
+                return 3
+            elif rate < buy2:
                 return 2
-            return 3
-        elif rate >= sell:
+            return 1
+        elif rate > sell:
             upper = row['upperband']
-            diff = upper - sell
-            if rate < upper:
-                return 1
-            if rate < upper + diff:
+            if rate > upper:
+                return 4
+            elif rate > sell3:
+                return 3
+            elif rate > sell2:
                 return 2
-            return 3
+            return 1
         return nan
 
     def plot(self, *args, **kwargs):
