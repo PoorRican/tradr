84c224bd3fe385c9cdb18255672e2671454fcf68
Josue Figueroa <poor.rican@pm.me>
2023-12-12 15:10:17 -0500

    Create `AssetGroup` for "update_candle_data" script

--- update_candle_data.py
+++ update_candle_data.py
@@ -1,21 +1,31 @@
 # This is a script to continually update the candle data for the given assets.
 # Data will be stored locally in the 'data' folder.
+from typing import Tuple, ClassVar
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from core import GeminiMarket
 
-ASSETS = ('btcusd', 'ethusd', 'dogeusd', 'dogeusd', 'avaxusd')
 
-def update_all():
-    for asset in ASSETS:
-        market = GeminiMarket(asset, '', '')
+class AssetGroup(object):
+    assets: ClassVar[Tuple[str, ...]] = ('btcusd', 'ethusd', 'dogeusd', 'dogeusd', 'avaxusd')
+
+    instances: Tuple[GeminiMarket, ...]
+
+    def __init__(self):
+        self.instances = tuple(GeminiMarket(asset, '', '') for asset in self.assets)
+
+    def update_all(self):
+        for instance in self.instances:
+            instance.update()
 
 if __name__ == '__main__':
+    group = AssetGroup()
+
     scheduler = BlockingScheduler()
     try:
         print('Starting...')
-        scheduler.add_job(update_all, 'interval', seconds=30)
+        scheduler.add_job(group.update_all, 'interval', seconds=30)
         scheduler.start()
     except KeyboardInterrupt:
         print("Shutting Down...")
